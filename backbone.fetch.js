49780bf8f061781c5fe7602ccd569ee262673b5c
Andrew Humphreys <ahumphreys87@googlemail.com>
2017-03-13 17:30:34 +0000

    ES6ify

--- backbone.fetch.js
+++ backbone.fetch.js
@@ -8,70 +8,62 @@
 
 import fetch from 'unfetch';
 
-(function() {
-  'use strict';
-
-  var defaults = function(obj, source) {
-    for (var prop in source) {
-      if (obj[prop] === undefined) obj[prop] = source[prop];
-    }
-    return obj;
-  };
-
-  var stringifyGETParams = function(url, data) {
-    var query = '';
-    for (var key in data) {
-      if (data[key] == null) continue;
-      query += '&'
-        + encodeURIComponent(key) + '='
-        + encodeURIComponent(data[key]);
-    }
-    if (query) url += (~url.indexOf('?') ? '&' : '?') + query.substring(1);
-    return url;
-  };
-
-  var getData = function(response, dataType) {
-    return dataType === 'json' ? response.json() : response.text();
-  };
-
-  var ajax = function(options) {
-    if (options.type === 'GET' && typeof options.data === 'object') {
-      options.url = stringifyGETParams(options.url, options.data);
-      delete options.data;
-    }
-
-    defaults(options, {
-      method: options.type,
-      headers: defaults(options.headers || {}, {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      }),
-      body: options.data
-    });
-
-    return fetch(options.url, options)
-      .then(function(response) {
-        var promise = getData(response, options.dataType);
-
-        if (response.ok) return promise;
-
-        var error = new Error(response.statusText);
-        return promise.then(function(responseData) {
-          error.response = response;
-          error.responseData = responseData;
-          if (options.error) options.error(error);
-          throw error;
-        });
-      })
-      .then(function(responseData) {
-        if (options.success) options.success(responseData);
-        return responseData;
-      });
-  };
-
-  if (typeof exports === 'object') {
-    module.exports = ajax;
-  } else {
-    Backbone.ajax = ajax;
+var defaults = function(obj, source) {
+  for (var prop in source) {
+    if (obj[prop] === undefined) obj[prop] = source[prop];
   }
-})();
+  return obj;
+};
+
+var stringifyGETParams = function(url, data) {
+  var query = '';
+  for (var key in data) {
+    if (data[key] == null) continue;
+    query += '&'
+      + encodeURIComponent(key) + '='
+      + encodeURIComponent(data[key]);
+  }
+  if (query) url += (~url.indexOf('?') ? '&' : '?') + query.substring(1);
+  return url;
+};
+
+var getData = function(response, dataType) {
+  return dataType === 'json' ? response.json() : response.text();
+};
+
+var ajax = function(options) {
+  if (options.type === 'GET' && typeof options.data === 'object') {
+    options.url = stringifyGETParams(options.url, options.data);
+    delete options.data;
+  }
+
+  defaults(options, {
+    method: options.type,
+    headers: defaults(options.headers || {}, {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    }),
+    body: options.data
+  });
+
+  return fetch(options.url, options)
+    .then(function(response) {
+      var promise = getData(response, options.dataType);
+
+      if (response.ok) return promise;
+
+      var error = new Error(response.statusText);
+      return promise.then(function(responseData) {
+        error.response = response;
+        error.responseData = responseData;
+        if (options.error) options.error(error);
+        throw error;
+      });
+    })
+    .then(function(responseData) {
+      if (options.success) options.success(responseData);
+      return responseData;
+    });
+};
+
+export default ajax;
